270c133e5bb09bc0e8b8df22965ce595779d66e8
RianovRV1 <potaco2@hotmail.com>
2018-05-06 14:24:30 -0700

    Finished POIObject
    Implemented workable POI

--- Assets/Scripts/POIObject.cs
+++ Assets/Scripts/POIObject.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(BoxCollider2D))]
 public class POIObject : MonoBehaviour {
 
-	// Use this for initialization
+    private Vector3 destination;
+    bool traveling;
+    // Use this for initialization
 	void Start () {
-		
+        traveling = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (traveling)
+        {
+            Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, destination, Time.deltaTime * 6);
+            if(Camera.main.transform.position == destination)
+            {
+                traveling = false;
+            }
+        }
+    }
     void OnMouseDown()
     {
-        Camera.main.transform.position = transform.position;
+        destination = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        traveling = true;
     }
 }
