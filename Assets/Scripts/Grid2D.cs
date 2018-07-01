86c37f66338f92535982e2b18023711ee789e711
RianovRV1 <potaco2@hotmail.com>
2018-07-01 03:17:24 -0700

    2D pathfinding
    Switched A* to unity 2d seciton

--- Assets/Scripts/Grid2D.cs
+++ Assets/Scripts/Grid2D.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour {
+public class Grid2D : MonoBehaviour {
 
     public LayerMask wallMask;
     public Vector2 gridWorldSize;
@@ -15,35 +15,38 @@ public class Grid : MonoBehaviour {
 
     float nodeDiameter;
     int gridSizeX, gridSizeY;
-	// Use this for initialization
-	private void Start ()
+    // Use this for initialization
+    private void Start()
     {
+        Distance = Mathf.Clamp(Distance, 0.1f, 100f);
+        nodeRadius =  Mathf.Clamp(nodeRadius, 0.5f, 100f);
         nodeDiameter = nodeRadius * 2;
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
         GenerateGrid();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     void GenerateGrid()//user made function to initialize grid
     {
         grid = new Node[gridSizeX, gridSizeY];
-        Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2; //Math to take the center position then find the bottomleft location of the grid
+        Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.up * gridWorldSize.y / 2; //Math to take the center position then find the bottomleft location of the grid
         for (int y = 0; y < gridSizeY; y++)
         {
             for (int x = 0; x < gridSizeX; x++)
             {
-                Vector3 worldPoint = bottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius); //math that starts at the bottomleft then adds vectors to get the nodes current location
+                Vector3 worldPoint = bottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (y * nodeDiameter + nodeRadius); //math that starts at the bottomleft then adds vectors to get the nodes current location
                 bool Wall = false;
                 bool wallColCheck = Physics.CheckSphere(worldPoint, nodeRadius, wallMask);
-                if(wallColCheck)
+                if (wallColCheck)
                 {
                     Wall = true;
                 }
-
+                Debug.Log("X: " + x + " Y: " + y);
                 grid[x, y] = new Node(Wall, worldPoint, x, y);
             }
         }
@@ -51,12 +54,12 @@ public class Grid : MonoBehaviour {
     internal Node NodeFromWorldPosition(Vector3 worldPosition)
     {
         float xpoint = ((worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x); // gets x position of grid node
-        float ypoint = ((worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y); // gets y position of grid node
+        float ypoint = ((worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y); // gets y position of grid node
         xpoint = Mathf.Clamp01(xpoint); // clamps it to prevent out of range errors
         ypoint = Mathf.Clamp01(ypoint); // clamps it to prevent out of range errors
         int x = Mathf.RoundToInt((gridSizeX - 1) * xpoint); // offsets it by 1 then uses X point poisition to get correct ARRAY INDEX
         int y = Mathf.RoundToInt((gridSizeY - 1) * ypoint); // offsets it by 1 then uses Y point poisition to get correct ARRAY INDEX
-        
+
         return grid[x, y];
     }
 
@@ -64,7 +67,7 @@ public class Grid : MonoBehaviour {
     {
         List<Node> neighboringNodes = new List<Node>();
         int xCheck, yCheck;
-        
+
         //Right Side Checking
         xCheck = startNode.xPos + 1;
         yCheck = startNode.yPos;
@@ -152,11 +155,11 @@ public class Grid : MonoBehaviour {
     }
     private void OnDrawGizmos()//this is for scene debugging to make sure our grid comes out the way we expect.
     {
-        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 1));
 
         if (grid != null)
         {
-            foreach(Node node in grid)
+            foreach (Node node in grid)
             {
                 if (node.isWall)
                 {
@@ -168,11 +171,11 @@ public class Grid : MonoBehaviour {
                 }
                 if (FinalPath != null)
                 {
-                    if(FinalPath.Contains(node))
+                    if (FinalPath.Contains(node))
                         Gizmos.color = Color.red; //color our node path
                 }
 
-                Gizmos.DrawCube(node.Position, Vector3.one * (nodeDiameter/2 - Distance)); //Draw our node
+                Gizmos.DrawCube(node.Position, Vector3.one * (nodeDiameter / 2 - Distance)); //Draw our node
             }
         }
     }
